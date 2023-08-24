0ee6de49d8233534d01e36d2384d54ef8610fcf4
jiraff537 <alexei.danilov@gmail.com>
2023-08-24 23:41:25 +0200

    +add error 4 igor to think about

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ import Classes.Cat;
 import Classes.Dog;
 import Classes.MilkPowered;
 import Classes.Snake;
+import interfaces.JivMIR;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        Cat catBarsik = new Cat("Orange", (byte) 14);
+//        Cat catBarsik = new Cat("Orange", (byte) 14);
+        JivMIR catBarsik = new Cat("Orange", (byte) 14);
         catBarsik.voice();
         catBarsik.move();
 
--- src/interfaces/JivMIR.java
+++ src/interfaces/JivMIR.java
@@ -1,7 +1,7 @@
 package interfaces;
 
 public interface JivMIR {
-    String move();
+    //String move();
 
     String voice();
 }
